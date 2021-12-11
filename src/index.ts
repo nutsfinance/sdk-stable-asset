0400fd6098dece94176421663232a3d46939843d
Shengda <dingshengda@gmail.com>
2021-12-11 00:05:08 -0800

    Export redeem result

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './errors';
 export * from './stable-asset';
 export * from './stable-swap-result';
 export * from './stable-mint-result';
+export * from './stable-redeem-proportion-result';
