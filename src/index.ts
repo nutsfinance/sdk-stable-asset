01d7942e8834e4cb7ca26e0b890899708373fc22
Shengda Ding <dingshengda@gmail.com>
2022-05-25 13:52:26 -0700

    Fix APR

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './errors';
 export * from './stable-asset';
+export * from './stable-asset-apr';
 export * from './stable-swap-result';
 export * from './stable-mint-result';
 export * from './stable-redeem-proportion-result';
