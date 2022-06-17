b7d5025417f3e933eebf69e6bc1dd2a9da500200
Shengda Ding <dingshengda@gmail.com>
2022-06-17 01:09:27 -0700

    Remove APR

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './errors';
 export * from './stable-asset';
-export * from './stable-asset-apr';
 export * from './swap-out-result';
 export * from './swap-in-result';
 export * from './mint-result';
