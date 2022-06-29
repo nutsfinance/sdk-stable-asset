a697ffc1b5081a7c8f9be5e683589428b5bf6cff
Shengda Ding <dingshengda@gmail.com>
2022-06-29 09:46:14 -0700

    Export single result

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './swap-out-result';
 export * from './swap-in-result';
 export * from './mint-result';
 export * from './redeem-proportion-result';
+export * from './redeem-single-result';
